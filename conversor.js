b56e9a0a53888027047d04981f5aa90087656fd6
Miguel Lucas Da Silva Farias <muguellucassilva@gmail.com>
2026-05-05 20:16:57 -0300

    formuilas

---
+++ conversor.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+function GrausPf(){
+    gF = GpF.value
+    GRAUSF=(gF*9/5)+32
+   RGpF.innerHTML = GRAUSF
+}
+ function GrausPk(){
+  gK = parseInt (GpK.value)
+  GRAUSK=(gK+273)
+  RGpK.innerHTML= GRAUSK
+ }
+ function fahrenheitPg(){
+  Fg = FpG.value
+  FARG = (Fg-32)*5/9
+ }
