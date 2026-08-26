29c617b09dfddb0f2babd2c3d26abce1cb5b91dc
Miguel Lucas Da Silva Farias <muguellucassilva@gmail.com>
2026-08-26 20:06:12 -0300

    trabalho funal

---
+++ aulas Em Python/Python_basico/aulapy9.py
@@ -0,0 +1,8 @@
+def calculo(x):
+    dobro = x * 2
+    return dobro
+def a():
+    num = int(input("digite um numero "))
+    resultado = calculo(num)
+    print(f"o numero digitado e {num} e seu dobro e {resultado}")
+a()
